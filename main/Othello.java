95a67bf4aa35b33055467821fe369186af1e1c8d
alesso425 <98488664+alesso425@users.noreply.github.com>
2024-04-04 11:04:58 -0400

    changes

--- main/Othello.java
+++ main/Othello.java
@@ -283,8 +283,56 @@ public class Othello {
         }
         printBoard();
     }
-    public ArrayList<int[]> allLegalMoves(boolean player){
+    public ArrayList<int[][]> allLegalMoves(boolean player)
+    {
+        int[] legalMove;
+        int[][] legalMoves;
+        if (player)
+        {
+            for (int i = 0; i <= board.length; i++)
+            {
+                for (int j = 0; j <= board[i].length; j++)
+                {
+                    if (isLegal(i, j))
+                    {
 
+                    }
+                }
+            }
+        }
+        else
+        {
+            for (int i = 0; i <= board.length; i++)
+            {
+                for (int j = 0; j <= board[i].length; j++)
+                {
+                    if (isLegal(i, j))
+                    {
+
+                    }
+                }
+            }
+        }
+        return legalMoves;
+    }
+
+    public boolean isLegal(int row, int column)
+    {
+        boolean con = false;
+        int [][] legal = getLegal(true, row, column);
+
+        for (int i = 0; i < legal.length; i++)
+        {
+            if (legal[i][0] != -1)
+            {
+                con = true;
+            }
+            else {
+                con = false;
+            }
+        }
+
+        return con;
     }
 
     public int[][] getLegal(boolean player, int row, int column){ //Makes a 2D-array for the location of legal moves left, up-left, up, up-right, right, down-right, down, down-left
