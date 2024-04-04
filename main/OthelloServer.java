f4a31c887a779b153b1dbe794ec31731a3067de5
blackbydefault <leroyabrother@gmail.com>
2024-04-04 11:18:01 -0400

    Update OthelloServer.java

--- main/OthelloServer.java
+++ main/OthelloServer.java
@@ -36,20 +36,23 @@ class ClientHandler extends Thread {
 			PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
 			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             out.println("[SERVER] >>> Are you new to the game?    Yes or No? ");
-			String clientResp = in.readLine();
+			String clientResp; 
+			while ((clientResp = in.readLine() != null)
+			{
+				if (clientResp.equals("yes"))
+				{
+					System.out.println("Client said yes.") //For Troubleshooting. Will comment after Othello 
+					//Create a new game (new Othello Object prompt player for  name and personal pass. 
+				}
+				else 
+				{
+					//Prompt player for existing user name and password to resume game.
+						//If input name and password is invalid, throw an exception to handle.
+				
+				
+				}
+			}
 			
-			if (clientResp.equals("yes"))
-			{
-				System.out.println("Client said yes.") //For Troubleshooting. Will comment after Othello 
-				//Create a new game (new Othello Object prompt player for  name and personal pass. 
-			}
-			else 
-			{
-				//Prompt player for existing user name and password to resume game.
-					//If input name and password is invalid, throw an exception to handle.
-				
-				
-			}
 			
 			
 			
@@ -71,7 +74,7 @@ class ClientHandler extends Thread {
 
             // Send acknowledgment to the client
 
-            out.println("ACK: Game state saved successfully!");
+           // out.println("ACK: Game state saved successfully!");
 
             clientSocket.close();
         } catch (IOException e) {
