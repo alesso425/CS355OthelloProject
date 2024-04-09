3afaa744b2bd362038e13f702a436910cd558c3f
alesso425 <98488664+alesso425@users.noreply.github.com>
2024-04-09 10:03:54 -0400

    Error Debugging

--- main/OthelloClient.java
+++ main/OthelloClient.java
@@ -1,5 +1,9 @@
 package main;
 
+import java.io.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class OthelloClient {
 	public static void main(String[] args) {
         try {
@@ -7,14 +11,11 @@ public class OthelloClient {
             System.out.println("Connected to game server. ");
 			//Game Setup.
 			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+			BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+
             String servResponse = in.readLine();
             System.out.println("[SERVER] : " + servResponse);
-			
-			
-			
-
-
+            
 			
             // Sends a request to save game state
             
@@ -22,15 +23,12 @@ public class OthelloClient {
             // out.println("SAVE_GAME");
 
             // Receive acknowledgment from the server
-         
-			
-			
-			
-			
 
-            //socket.close();
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
+        //socket.close();
     }
 }
